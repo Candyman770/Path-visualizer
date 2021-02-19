87e8254b1a3814eaf6a380a0a509928f0027cae5
Candyman770 <isaac2311.cl@gmail.com>
2021-02-20 03:41:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./App.css";
+import Visualizer from './visualizer/visualizer';
+
+const App=()=>{
+    return (
+    	<Visualizer />
+    );
+}
+
+export default App;
